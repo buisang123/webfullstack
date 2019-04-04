342ef4183102c70e4a2adb76c3a17bb934d8d1c2
sangbui <bducsang123@gmail.com>
2019-04-04 22:23:46 +0700

    nop bai

---
+++ sever.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+app.listen(6699 ,function(error){
+    if(error) console.log(error)
+    else console.log("sever start success !!")
+});
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+app.get('/:Webnumber', function(req, res){
+    const Webnumber = req.params.Webnumber;
+    
+    try{
+        const data = JSON.parse(fs.readFileSync(__dirname + "/data/" +Webnumber + ".json", { encoding: "utf-8" }))
+        let output = `<ul>`;
+        data.map(function(name, key){
+                output += `<li>${name}</li>`;
+            });
+        output += `</ul>`;
+        res.send(output);
+    } catch (error){
+        res.send("404 Not Found!");
+    }
+    
+    
+})
